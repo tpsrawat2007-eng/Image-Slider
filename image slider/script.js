ea3208ee9c73b564acf7febeef02257db27d7127
tpsrawat2007-eng <tpsrawat2007@gmail.com>
2025-12-10 13:43:01 +0530

    Image Slider

---
+++ image slider/script.js
@@ -0,0 +1,35 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.image');
+
+let slideNumber = 1;  
+const length = images.length;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+    slideNumber++;
+};
+
+const prevSlide = () => {
+    slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+    slideNumber--;
+};
+
+const getFirstSlide = () => {
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+};
+
+const getLastSlide = () => {
+    slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+    slideNumber = length;
+};
+
+right.addEventListener('click', () => {
+    slideNumber < length ? nextSlide() : getFirstSlide();
+});
+
+left.addEventListener('click', () => {
+    slideNumber > 1 ? prevSlide() : getLastSlide();
+});
